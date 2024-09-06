957db12c3dc391222b1fb487c9986de2036325a9
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2024-09-06 16:50:00 +0200

    Hero

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,37 @@
+import { benefits } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
+
+        <div className="flex fle-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat 
+                    bg-[length:100%_100%] md:ma-w-[24rem]
+                    "
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem]">
+                <h5>{item.title}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
+
