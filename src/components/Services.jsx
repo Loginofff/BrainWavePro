76491a29cddfb522559a44b13b2011e9acd8a4af
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2024-09-11 21:23:58 +0200

    PricingList

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -19,9 +19,10 @@ const Services = () => {
           title="Generative AI made for creators."
           text="Brainwave unlocks the potential of AI-powered applications"
         />
-
+ 
         <div className="relative">
-          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border
+           border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <img
                 className="w-full h-full object-cover md:object-right"
@@ -50,7 +51,8 @@ const Services = () => {
               </ul>
             </div>
 
-            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 
+            border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
           </div>
 
           <div className="relative z-1 grid gap-5 lg:grid-cols-2">
@@ -64,7 +66,7 @@ const Services = () => {
                   alt="robot"
                 />
               </div>
-
+ 
               <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
                 <h4 className="h4 mb-4">Photo editing</h4>
                 <p className="body-2 mb-[3rem] text-n-3">
